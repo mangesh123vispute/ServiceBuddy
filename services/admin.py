5883b7b08a888672ad61beeb637a3127a6ab269c
MangeshVispute <mangesh.vispute@networcx.io>
2025-03-17 21:51:51 +0530

    updated admin.py file

--- services/admin.py
+++ services/admin.py
@@ -8,23 +8,23 @@ class ServiceProviderResource(resources.ModelResource):
     service = fields.Field(
         column_name='service',
         attribute='service',
-        widget=ForeignKeyWidget(Service, field='name')  # Maps service name instead of ID
+        widget=ForeignKeyWidget(Service, field='id')  # Maps directly using service ID
     )
 
     class Meta:
         model = ServiceProvider
         fields = ('id', 'name', 'description', 'service', 'rating', 'availability', 'experience')
 
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'icon_link')
-
 @admin.register(ServiceProvider)
 class ServiceProviderAdmin(ImportExportModelAdmin):
     resource_class = ServiceProviderResource
     list_display = ('name', 'service', 'rating', 'availability', 'experience')
-    search_fields = ('name', 'service__name')
+    search_fields = ('name', 'service__id')
     list_filter = ('availability', 'service')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon_link')
 
 @admin.register(GlobalSettings)
 class GlobalSettingsAdmin(admin.ModelAdmin):
