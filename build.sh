9964df1f2af36f641e9c547d7519333d367675c2
MangeshVispute <mangesh.vispute@networcx.io>
2025-03-22 19:08:36 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,16 @@ pip install -r req.txt
 python manage.py collectstatic --noinput
 
 # Run migrations
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser (if not exists)
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "1234")
+    print("Superuser created!")
+else:
+    print("Superuser already exists.")
+EOF
