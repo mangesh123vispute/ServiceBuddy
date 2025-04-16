aaca7fe38fdfd2424383e794e0217407e9120e9e
MangeshVispute <mangesh.vispute@networcx.io>
2025-04-16 08:19:29 +0530

    change header from admin page

---
+++ serviceProviders/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+
+# Customize admin site header and title
+admin.site.site_header = 'Seva Enterprises'
+admin.site.site_title = 'Seva Enterprises'
+admin.site.index_title = 'Welcome to Seva Enterprises Administration' 
