b1d29c32b76a16a80825850e7ab8d8fd9a5defd2
MangeshVispute <mangesh.vispute@networcx.io>
2025-04-27 14:19:10 +0530

    added url for create comment

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ServiceListView, ServiceProviderListView, ServiceProviderDetailView,CustomerRequestCreateView
+from .views import ServiceListView, ServiceProviderListView, ServiceProviderDetailView, CustomerRequestCreateView, CommentsCreateView
 
 urlpatterns = [
     path('services/', ServiceListView.as_view(), name='service-list'),
     path('services/<str:service_name>/', ServiceProviderListView.as_view(), name='service-provider-list'),
     path('services/<str:service_name>/<int:service_provider_id>/', ServiceProviderDetailView.as_view(), name='service-provider-detail'),
     path('customer-request/', CustomerRequestCreateView.as_view(), name='customer-request-create'),
+    path('comments/', CommentsCreateView.as_view(), name='comments-create'),
 ]
